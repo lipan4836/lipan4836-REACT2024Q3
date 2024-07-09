459b9e765b51f90d5801cfc074d891eeb9722906
Anton <lipan48@gmail.com>
2024-07-09 16:53:50 +0300

    task#2/feat: implement base router

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,12 +1,21 @@
+import { useNavigate } from 'react-router-dom';
 import './NotFoundPage.scss';
 
 function NotFoundPage() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="notFoundPageWrap">
       <div className="notFoundPage">
         <div className="notFoundPage_img"></div>
         <p className="notFoundPage_msg">Looks like you were lost. There is no such page.</p>
-        <button className="notFoundPage_btn">Go back</button>
+        <button className="notFoundPage_btn" onClick={goBack}>
+          Go back
+        </button>
       </div>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx';
 import './index.scss';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import ErrorContent from './components/ErrorBoundary/ErrorContent.tsx';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorContent error={null} errorInfo={null} />,
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<ErrorContent error={null} errorInfo={null} />}>
-      <App />
+      <RouterProvider router={router} />
     </ErrorBoundary>
   </React.StrictMode>,
 );
