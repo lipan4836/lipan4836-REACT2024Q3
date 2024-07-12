3f5f8b233d8969a1a000d62ef58af0956586ecbd
Anton <lipan48@gmail.com>
2024-07-12 15:21:37 +0300

    task#2/feat: complete logic of card list and card detail page

--- src/components/CardDetail/CardDetail.tsx
+++ src/components/CardDetail/CardDetail.tsx
@@ -5,6 +5,7 @@ import './CardDetail.scss';
 import { fetchDataById } from '../../api/api';
 import Loader from '../Loader/Loader';
 import NoPhoto from '../NoPhoto/NoPhoto';
+import closeBtn from '../../assets/svg/close.svg';
 
 function CardDetail() {
   const { id, pageId } = useParams<{ id: string; pageId: string }>(); // Получаем параметр id из адресной строки
@@ -47,34 +48,49 @@ function CardDetail() {
   if (!character) return <div>No character found</div>;
 
   return (
-    <div className="characterDetails">
-      <button className="closePage" onClick={handleClosePage}>
-        close
-      </button>
-      <h2 className="char_name">{character.name}</h2>
-      {character.images.length ? (
-        <img src={character.images[0]} alt={character.name} className="char_img" />
-      ) : (
-        <NoPhoto />
-      )}
-      <div className="char_personal">
-        {character.personal.birthdate && (
-          <div className="char_personal_box">
-            <p className="char_personal_box_data">
-              <strong>Birthdate:</strong> {character.personal.birthdate}
-            </p>
-          </div>
+    <div className="detailsWrap">
+      <div className="characterDetails">
+        <button className="closePage" onClick={handleClosePage}>
+          <img src={closeBtn} alt="close details" className="closePage_img" />
+        </button>
+        <h2 className="charDetail_name">{character.name}</h2>
+        {character.images.length ? (
+          <img src={character.images[0]} alt={character.name} className="char_img" />
+        ) : (
+          <NoPhoto />
         )}
-        <p className="char_personal_box_data">
-          <strong>Sex:</strong> {character.personal.sex}
-        </p>
-        {character.personal.clan && (
-          <div className="char_personal_box">
-            <p className="char_personal_box_data">
-              <strong>Clan:</strong> {character.personal.clan}
-            </p>
-          </div>
-        )}
+        <div className="charDetail_personal">
+          {character.personal.birthdate && (
+            <div className="charDetail_personal_box">
+              <p className="charDetail_personal_box_data">
+                <strong>Birthdate:</strong> {character.personal.birthdate}
+              </p>
+            </div>
+          )}
+          <p className="charDetail_personal_box_data">
+            <strong>Sex:</strong> {character.personal.sex}
+          </p>
+          {character.personal.clan && (
+            <div className="charDetail_personal_box">
+              <p className="charDetail_personal_box_data">
+                <strong>Clan:</strong> {character.personal.clan}
+              </p>
+            </div>
+          )}
+          {character.debut && <p className="charDetail_debut">Debut:</p>}
+          {character.debut?.anime && (
+            <p className="charDetail_debut_type">Anime: {character.debut.anime}</p>
+          )}
+          {character.debut?.manga && (
+            <p className="charDetail_debut_type">Manga: {character.debut.manga}</p>
+          )}
+          {character.debut?.novel && (
+            <p className="charDetail_debut_type">Novel: {character.debut.novel}</p>
+          )}
+          {character.debut?.game && (
+            <p className="charDetail_debut_type">Game: {character.debut.game}</p>
+          )}
+        </div>
       </div>
     </div>
   );
