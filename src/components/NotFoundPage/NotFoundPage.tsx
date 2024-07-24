47e205399535bdb10fb024bb7e981f9b7b194b61
Anton <lipan48@gmail.com>
2024-07-24 04:00:34 +0300

    task#3/feat: implement dark theme for NotFoundPage and little styles fix in NotFoundChar

--- src/components/NotFoundPage/NotFoundPage.tsx
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -1,15 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import './NotFoundPage.scss';
+import useAppContext from '../AppContext/useAppContext';
 
 function NotFoundPage() {
   const navigate = useNavigate();
+  const { darkTheme } = useAppContext();
 
   const goBack = () => {
     navigate(-1);
   };
 
   return (
-    <div className="notFoundPageWrap">
+    <article className={darkTheme ? 'notFoundPageWrap darkWrap' : 'notFoundPageWrap'}>
       <div className="notFoundPage">
         <div className="notFoundPage_img"></div>
         <p className="notFoundPage_msg">Looks like you were lost. There is no such page.</p>
@@ -17,7 +19,7 @@ function NotFoundPage() {
           Go back
         </button>
       </div>
-    </div>
+    </article>
   );
 }
 
